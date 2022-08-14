95b5830c138393d9a2bb4b4e630d46be90ac036c
johanpark <pyhan23@gmail.com>
2022-08-15 00:51:48 +0900

    first commit

---
+++ src/test/java/com/example/jdbcstudy/connection/DBConnectionUtilTest.java
@@ -0,0 +1,18 @@
+package com.example.jdbcstudy.connection;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+
+    }
+}
